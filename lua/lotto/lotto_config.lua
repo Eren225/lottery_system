c05bafe566d13017836fdaecea3d8ab3e97356ab
Eren225 <95093309+Eren225@users.noreply.github.com>
2023-05-24 16:48:32 +0200

    Update lotto_config.lua

--- lua/lotto/lotto_config.lua
+++ lua/lotto/lotto_config.lua
@@ -1,20 +1,20 @@
 ErenConfig = ErenConfig or {}
 
-ErenConfig.closeTime = 3*60 // time for the mimenu to close
+ErenConfig.closeTime = 3*60 // time for the menu to close
 
 ErenConfig.winAmount = 5000 // amount won for the winner for 
 
-ErenConfig.joinCost = 5000
+ErenConfig.joinCost = 5000 // cost to join the lotery
 
-ErenConfig.leaderWin = 1000
+ErenConfig.leaderWin = 1000 // how much does the winner earns per player
 
-ErenConfig.tempsChoixGagnant = 3*60 
+ErenConfig.tempsChoixGagnant = 3*60 // time before knowing who wins
 
-ErenConfig.cooldownLotto = 6*60
+ErenConfig.cooldownLotto = 6*60 // cooldown between each lotery
 
-ErenConfig.prefix = "!loterie"
+ErenConfig.prefix = "!loterie" // prefix to open the lotery menu as a leader
 
-ErenConfig.leaderTeam = {
+ErenConfig.leaderTeam = {  // teams allowed to open the menu
     [TEAM_CITIZEN] = true,
     [TEAM_HOBO] = true,
 
